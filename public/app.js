e7219310ffde755b14554af4f916f4c5eb7b531e
Shivakumar S N <shivu200500@gmail.com>
2026-05-02 19:10:02 +0530

    Fix HTML anchor tag for college logo in app.js

--- public/app.js
+++ public/app.js
@@ -139,7 +139,7 @@ function authView() {
   app.innerHTML = `
     <section class="auth-layout">
       <div class="auth-visual">
-        <a href="https://images-hostinger.suggestcollege.com//media/colleges/cover/colleges/logo/1486619310d1_mqJ9RVC.jpg"> KITT <a/>
+<img src="/campus-registration.png?v=4" alt="Students registering at a college competition desk" />
         <div class="brand-block">
           <div class="brand-kicker">${COLLEGE_NAME}</div>
           <h1>${FEST_NAME}</h1>
